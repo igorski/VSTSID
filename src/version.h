ca518d3df4f548bac99d00165f35df680cd53617
Igor Zinken <igor.zinken@igorski.nl>
2020-08-30 10:30:55 +0200

    Made all plugin version definitions based in the makefile

--- src/version.h
+++ src/version.h
@@ -1,19 +1,19 @@
 #ifndef __VERSION_HEADER__
 #define __VERSION_HEADER__
 
-#include "pluginterfaces/base/fplatform.h"
+#define INT_TO_STR(x) #x
 
-#define MAJOR_VERSION_STR "1"
-#define MAJOR_VERSION_INT 1
+#define MAJOR_VERSION_INT PLUGIN_MAJOR_VERSION
+#define MAJOR_VERSION_STR INT_TO_STR(MAJOR_VERSION_INT)
 
-#define SUB_VERSION_STR "3"
-#define SUB_VERSION_INT 3
+#define SUB_VERSION_INT PLUGIN_MINOR_VERSION
+#define SUB_VERSION_STR INT_TO_STR(SUB_VERSION_INT)
 
-#define RELEASE_NUMBER_STR "0"
-#define RELEASE_NUMBER_INT 1
+#define RELEASE_NUMBER_INT PLUGIN_RELEASE_NUMBER
+#define RELEASE_NUMBER_STR INT_TO_STR(RELEASE_NUMBER_INT)
 
-#define BUILD_NUMBER_STR "2" // Build number to be sure that each result could identified.
-#define BUILD_NUMBER_INT 2
+#define BUILD_NUMBER_INT PLUGIN_BUILD_NUMBER
+#define BUILD_NUMBER_STR INT_TO_STR(PLUGIN_BUILD_NUMBER)
 
 // Version with build number (example "1.0.3.342")
 #define FULL_VERSION_STR MAJOR_VERSION_STR "." SUB_VERSION_STR "." RELEASE_NUMBER_STR "." BUILD_NUMBER_STR
@@ -23,12 +23,12 @@
 
 #define stringOriginalFilename  "vstsid.vst3"
 #if PLATFORM_64
-#define stringFileDescription   "VSTSID VST3-SDK (64Bit)"
+#define stringFileDescription   "VSTSID plugin (64Bit)"
 #else
-#define stringFileDescription   "VSTSID VST3-SDK"
+#define stringFileDescription   "VSTSID plugin"
 #endif
 #define stringCompanyName       "igorski.nl\0"
-#define stringLegalCopyright    "� 2018 igorski.nl"
+#define stringLegalCopyright    #PLUGIN_COPYRIGHT
 #define stringLegalTrademarks   "VST is a trademark of Steinberg Media Technologies GmbH"
 
 #endif
