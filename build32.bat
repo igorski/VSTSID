13d8e44eca5bef2eb6fff1cfc8602fe7b9077c94
Igor Zinken <igor.zinken@gmail.com>
2024-12-27 10:23:15 +0100

    Update to support SDK 3.7.11 via local installation. Integrate AUv3 support

--- build32.bat
+++ build32.bat
@@ -2,7 +2,16 @@
 rmdir /Q /S build
 mkdir build
 cd build
-cmake -G "Visual Studio 16 2019" -A Win32 -S .. -DSMTG_CREATE_VST2_VERSION=ON -DVST3_SDK_ROOT=%VST3_SDK_ROOT_32BIT%
-cmake --build . --config Release
-rename VST3\Release\vstsid.vst3\Contents\x86-win\vstsid.vst3 vstsid.dll
+
+if not defined VST3_SDK_ROOT (
+    set VST3_SDK_ROOT=%CD%\vst3sdk
+)
+
+echo Building using VST3 SDK located at %VST3_SDK_ROOT%
+
+cmake -G "Visual Studio 16 2019" -A Win32 -S .. -B "build32" -DSMTG_CREATE_VST2_VERSION=ON -DVST3_SDK_ROOT=%VST3_SDK_ROOT_32BIT%
+cmake --build build32 --config Release
+
+rename build32\VST3\Release\vstsid.vst3\Contents\x86-win\vstsid.vst3 vstsid.dll
+
 cd ..
